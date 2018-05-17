ada8fece7ccc587c5408dec307bc66457cdca5e9
hys <huyansheng@fishtrip.cn>
2018-05-17 18:54:14 +0800

    fix: 处理 csp 逻辑

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import { swInit } from './service-client'
+// import { swInit } from './service-client'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
-swInit()
+// swInit()
